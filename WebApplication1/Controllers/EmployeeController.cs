33f1bf0d17a5961795fb60e948a19b52dea73192
zahidul5784@gmail.com <zahidul5784@gmail.com>
2024-01-30 19:16:07 +0600

    Add project files.

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	//[EnableCors()]
+	public class EmployeeController : ControllerBase
+	{
+		static List<Employee> data = new List<Employee>()
+		{
+			new Employee(){empId =1, empName ="Employee 1", address ="address 1" },
+			new Employee{empId =2, empName ="Employee 2", address ="address 2" },
+			new Employee{empId =3, empName ="Employee 3", address ="address 3" }
+		};
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok(data);
+		}
+
+		[HttpPost]
+		public IActionResult Post(Employee e)
+		{
+			e.empId = data.Count > 0 ? data.Max(e => e.empId) + 1 : 1;
+
+
+			data.Add(e);
+
+			return Ok(e);
+		}
+
+	}
+}
---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+	public class Employee
+	{
+        public int empId { get; set; }
+        public string? empName { get; set; }
+        public string? address { get; set; }
+	}
+}
